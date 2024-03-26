55d313bdbf73e0065e85fff593fe776b2c7a39d6
HyukSang <zzooppii@gmail.com>
2024-03-26 16:29:06 +0900

    upgrade initialize

--- contracts/L2/L2FastWithdrawProxy.sol
+++ contracts/L2/L2FastWithdrawProxy.sol
@@ -6,4 +6,19 @@ import { L2FastWithdrawStorage } from "./L2FastWithdrawStorage.sol";
 
 contract L2FastWithdrawProxy is Proxy, L2FastWithdrawStorage {
 
+    function initialize(
+        address _crossDomainMessenger,
+        address _l1fastWithdraw,
+        address _legacyERC20,
+        address _l1legacyERC20
+    ) 
+        external
+        onlyOwner
+    {
+        crossDomainMessenger = _crossDomainMessenger;
+        l1fastWithdrawContract = _l1fastWithdraw;
+        LEGACY_ERC20_ETH = _legacyERC20;
+        enteringToken[_legacyERC20] = _l1legacyERC20;
+        checkToken[_l1legacyERC20][_legacyERC20] = true;
+    }
 }
