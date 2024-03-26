51b2fda203f9ef15a4c0c07e541ae19c3a23a281
HyukSang <zzooppii@gmail.com>
2024-03-26 16:29:15 +0900

    update L2Storage

--- contracts/L2/L2FastWithdrawStorage.sol
+++ contracts/L2/L2FastWithdrawStorage.sol
@@ -2,5 +2,22 @@
 pragma solidity 0.8.20;
 
 contract L2FastWithdrawStorage {
+    struct RequestData {
+        address l2token;
+        address seller;
+        address buyer;
+        uint256 sellAmount;
+        uint256 minAmount;
+        uint256 buyAmount;
+    }
 
+    address public crossDomainMessenger;
+    address public l1fastWithdrawContract;
+    address public LEGACY_ERC20_ETH;
+
+    uint256 public salecount;
+
+    mapping(uint256 => RequestData) public dealData;
+    mapping(address => address) public enteringToken;
+    mapping(address => mapping(address => bool)) public checkToken;
 }
