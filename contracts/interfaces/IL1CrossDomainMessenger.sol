89d90f543e0c1be2126c2eefdbb19993b114f209
HyukSang <zzooppii@gmail.com>
2024-03-28 12:33:50 +0900

    add interface

---
+++ contracts/interfaces/IL1CrossDomainMessenger.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IL1CrossDomainMessenger {
+    function sendNativeTokenMessage(
+        address _target,
+        uint256 _amount,
+        bytes calldata _message,
+        uint32 _minGasLimit
+    )
+        external;
+}
