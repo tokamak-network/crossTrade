ff3753aebbe0f68678323e57d88794294bc554e1
HyukSang <zzooppii@gmail.com>
2024-04-11 01:08:02 +0900

    Create MockERC20Token.sol

---
+++ contracts/MockERC20Token.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.20;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20Token is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    }
+
+    function mint(address addr, uint256 amount) external {
+        _mint(addr, amount);
+    }
+}
