9c891b4f80f844da3924aa53d134a4c090effa8c
HyukSang <zzooppii@gmail.com>
2024-06-12 19:15:51 +0900

    test Preparing for the test #21
    New L1 and L2 test environment settings have been created locally to apply the test environment.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,7 @@ const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.24',
     settings: {
-      evmVersion: "cancun",
+      // evmVersion: "cancun",
       viaIR: true,
       optimizer: {
           enabled: true,
